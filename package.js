6e5c2bcb21ae368c4c1e748e74be5da6e4a8bde3
invictus <a.hanel@xhttp.de>
2015-11-01 22:38:59 +0100

    summary added

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
     name: 'invictus:messages',
     version: '0.9.0',
     // Brief, one-line summary of the package.
-    summary: '',
+    summary: 'Boostrap Messages',
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'https://github.com/xhttpdev/meteor-messages.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
